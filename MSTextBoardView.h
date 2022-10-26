aa06bad1ee22926dfefcf06d0397071bb9008b54
Mariusz Woźniak <116265488+264414@users.noreply.github.com>
2022-10-26 06:23:44 +0200

    Create MSTextBoardView.h

---
+++ MSTextBoardView.h
@@ -0,0 +1,18 @@
+#include "Minesweeper.h"
+
+#ifndef UNTITLED_MSTEXTBOARDVIEW_H
+#define UNTITLED_MSTEXTBOARDVIEW_H
+
+
+class MSTextBoardView {
+
+public:
+    MSTextBoardView(Minesweeper m1);
+    MSTextBoardView();
+    void GameDisplay(Minesweeper m1);
+    void GameDisplayVideo(Minesweeper m1);
+
+};
+
+
+#endif //UNTITLED_MSTEXTBOARDVIEW_H
