2cae23e1c4c5008ac045e1d6a3912dfcda827b3f
Mariusz Woźniak <116265488+264414@users.noreply.github.com>
2022-10-26 06:24:26 +0200

    Create Variables.h

---
+++ Variables.h
@@ -0,0 +1,25 @@
+#ifndef UNTITLED_VARIABLES_H
+#define UNTITLED_VARIABLES_H
+
+#define MAX_LEVEL 4
+#define MAX_WIDTH 100
+#define MAX_HEIGHT 100
+
+#define EASY 1
+#define NORMAL 2
+#define HARD 3
+#define DEBUG 4
+
+//Rozmiar gry
+#define ROZMIAR 40
+#define ROZMIAR_START 1
+
+
+#define MIN_LEVEL 1
+#define MAX_LEVEL 4
+#define MIN_WIDTH 5
+#define MIN_HEIGHT 5
+
+
+
+#endif //UNTITLED_VARIABLES_H
